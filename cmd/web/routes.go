18e31a3f9c27b7524a3e27d7b0635f25a8b1f166
ozan1338 <akhmadfauzan1338@gmail.com>
2022-12-12 22:31:13 +0700

    First Commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/bmizerany/pat"
+	"net/http"
+	"ws/internal/handlers"
+)
+
+// routes defines the application routes
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	return mux
+}
